06c4f4e059bf2d0812c1fd42cbf4253361d3b46b
jfsanzag-alt <jfsanzag@gmail.com>
2026-02-10 21:58:29 +0000

    JFSA ejercicio

---
+++ 01_desafios_texto/mi_anagrama.py
@@ -0,0 +1,10 @@
+# Son anagramas ramo y mora
+def es_anagrama(pal1, pal2):
+    pal1 = "ramo"
+    print(pal2[-2:])
+    print(pal2[0:2])
+    print(pal2[2:4])
+
+    return True
+
+print(es_anagrama("pedo", "ramo"))

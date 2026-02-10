bc4c966b2a817b2fb03ad062634295daf05451ce
jfsanzag-alt <jfsanzag@gmail.com>
2026-02-10 19:07:08 +0000

    JFSA ejercicio

--- 01_desafios_texto/conver_format_time.py
+++ 01_desafios_texto/conver_format_time.py
@@ -1,25 +1,23 @@
-from datetime import datetime
-
 def convierte_hora(hora12):
-  hora24=""
-  
-  ind_am_pm = hora12[6] + hora12[7]
-  print(ind_am_pm)
+  ind_am_pm = hora12[-2:].lower()
 
-  if ind_am_pm == "PM": 
-    hora=str(int(hora12[0:2])+12)
+  hora = hora12[:2]
+  if ind_am_pm == "pm":
+    if hora12[:2] != "12":
+      hora=str(int(hora12[0:2])+12)
   else:
-    hora = hora12[0:2]
+    if hora12[:2] == "12":
+      hora = "00"
 
   min = hora12[3:5]
-  print(hora)
-  print(min)
 
   hora24 = hora + ":" + min
 
   return hora24
 
+#print(hora12.split(":")) Pasa hora12 a una lista ['05', '45pm']
+#hora_convertida = ":".join(hora_lista) une los elem de hora lista separados por ":" 05:45pm
 
-hora12="05:25 AM"
-print(hora12)
-print(convierte_hora(hora12))
+print(convierte_hora("12:40AM")) # 00:40
+print(convierte_hora("04:59pm")) # 16:59
+print(convierte_hora("10:00PM")) # 22:00

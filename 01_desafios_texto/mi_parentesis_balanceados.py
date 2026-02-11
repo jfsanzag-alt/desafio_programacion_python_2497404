6b40866c3ca3720a92f82627e12fed72d2a2da0c
jfsanzag-alt <jfsanzag@gmail.com>
2026-02-11 13:15:12 +0000

    JFSA correccion

--- 01_desafios_texto/mi_parentesis_balanceados.py
+++ 01_desafios_texto/mi_parentesis_balanceados.py
@@ -1,12 +1,32 @@
 def parentesis_balanceados(cadena):
     
+    # No detecto el caso de apertura incorrecta ")("
+    '''
     num_par_de = cadena.count(")")
     num_par_iz = cadena.count("(")
     print(num_par_iz, num_par_de)
 
     return num_par_iz == num_par_de
+    '''
+
+    #Algoritmo correcto
+    i = 0
+    num_par = 0
+    while i < len(cadena):
+        if cadena[i] == "(":
+            num_par += 1
+        elif cadena[i] == ")":
+            num_par -= 1
+            if num_par < 0:
+                return False
+        i += 1
+
+    return (num_par == 0)
+
 
 print(parentesis_balanceados("(()())()"))  #true
 print(parentesis_balanceados("() (())) ()")) #false
 
-# No detecto el caso de apertura incorrecta ")("
+print(parentesis_balanceados("((()))()"))  #true
+print(parentesis_balanceados(")(()"))  #false
+print(parentesis_balanceados("(()"))  #false
